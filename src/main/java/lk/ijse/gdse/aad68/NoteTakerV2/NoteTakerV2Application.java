c97bbfde4d6888b3a5d19af2bd0f05a116cfe5d9
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-06 10:51:23 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/NoteTakerV2Application.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.aad68.NoteTakerV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakerV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteTakerV2Application.class, args);
+	}
+
+}
