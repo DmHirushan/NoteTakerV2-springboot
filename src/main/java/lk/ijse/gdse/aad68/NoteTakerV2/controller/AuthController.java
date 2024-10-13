97819c7ede686e75420cca8e00bec749b153f874
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-13 12:25:26 +0530

    auth controller created

--- src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/controller/AuthController.java
+++ src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/controller/AuthController.java
@@ -1,9 +1,20 @@
 package lk.ijse.gdse.aad68.NoteTakerV2.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthController {
+    @PostMapping(value = "signin")
+    public String signIn(){
+        return "signIn";
+    }
+
+    @PostMapping(value = "signup")
+    public String signUp(){
+        return "signUp";
+    }
+
 }
