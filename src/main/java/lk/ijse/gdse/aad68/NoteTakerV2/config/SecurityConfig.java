1efca337adeab6801c6bc40c040a7789af55582b
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-13 13:10:07 +0530

    securityFilterChain function created

--- src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/config/SecurityConfig.java
+++ src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/config/SecurityConfig.java
@@ -1,4 +1,27 @@
 package lk.ijse.gdse.aad68.NoteTakerV2.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionCreationEvent;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(req ->
+                        req.requestMatchers("api/v1/auth/**").permitAll()
+                                .anyRequest()
+                                .authenticated()
+                ).sessionManagement(SessionCreationPolicy.STATELESS)
+                .authenticationProvider()
+                .addFilterBefore();
+        return httpSecurity.build();
+    }
 }
