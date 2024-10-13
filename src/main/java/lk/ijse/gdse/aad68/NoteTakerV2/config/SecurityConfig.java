98db2c7a7683d9cd225e464b6454e72b5b027859
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-13 13:28:06 +0530

    main beans implemented

--- src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/config/SecurityConfig.java
+++ src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/config/SecurityConfig.java
@@ -1,27 +1,54 @@
 package lk.ijse.gdse.aad68.NoteTakerV2.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionCreationEvent;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests(req ->
                         req.requestMatchers("api/v1/auth/**").permitAll()
                                 .anyRequest()
                                 .authenticated()
-                ).sessionManagement(SessionCreationPolicy.STATELESS)
+                ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider()
                 .addFilterBefore();
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //Auth provider
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider dao = new DaoAuthenticationProvider();
+        dao.setUserDetailsService();
+        dao.setPasswordEncoder(passwordEncoder());
+        return dao;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
