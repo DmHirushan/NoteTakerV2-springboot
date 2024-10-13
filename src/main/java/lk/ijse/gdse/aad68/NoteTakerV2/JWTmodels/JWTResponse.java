394dd128d8dad769504937ab8d9de54729255728
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-13 12:35:45 +0530

    constructors | getters setters added

--- src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/JWTmodels/JWTResponse.java
+++ src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/JWTmodels/JWTResponse.java
@@ -1,4 +1,11 @@
 package lk.ijse.gdse.aad68.NoteTakerV2.JWTmodels;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class JWTResponse {
 }
