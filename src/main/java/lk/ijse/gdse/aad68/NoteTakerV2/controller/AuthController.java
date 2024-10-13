34eadfdd433530816e0d727db4afe2d8f94a76be
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-13 13:12:04 +0530

    bug fixed

--- src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/controller/AuthController.java
+++ src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/controller/AuthController.java
@@ -1,20 +1,49 @@
 package lk.ijse.gdse.aad68.NoteTakerV2.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.gdse.aad68.NoteTakerV2.JWTmodels.JWTResponse;
+import lk.ijse.gdse.aad68.NoteTakerV2.JWTmodels.SignIn;
+import lk.ijse.gdse.aad68.NoteTakerV2.dto.impl.UserDto;
+import lk.ijse.gdse.aad68.NoteTakerV2.exception.DataPersistFailedException;
+import lk.ijse.gdse.aad68.NoteTakerV2.util.AppUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthController {
 
-    @PostMapping(value = "signup")
-    public String signUp(){
-        return "signUp";
+    @PostMapping(value = "signup", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<JWTResponse> signUp(@RequestPart("firstName") String firstName,
+                                              @RequestPart("lastName") String lastName,
+                                              @RequestPart("email") String email,
+                                              @RequestPart("password") String password,
+                                              @RequestPart("profilePic") MultipartFile profilePic){
+
+        try{
+            //Handle profile pic
+            byte[] imageByteCollection = profilePic.getBytes();
+            String base64ProfilePic = AppUtil.toBase64ProfilePic(imageByteCollection);
+            var buildUserDto = new UserDto();
+            buildUserDto.setFirstName(firstName);
+            buildUserDto.setLastName(lastName);
+            buildUserDto.setEmail(email);
+            buildUserDto.setPassword(password);
+            buildUserDto.setProfilePic(base64ProfilePic);
+
+            userService.saveUser(buildUserDto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistFailedException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping(value = "signin")
-    public String signIn(){
+    public String signIn(@RequestBody SignIn signIn){
         return "signIn";
     }
 
