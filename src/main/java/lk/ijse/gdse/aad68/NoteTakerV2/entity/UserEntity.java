8d220485ce31ec22b6b204a9b4e651bdd636fd09
DmHirushan <dimuthsamaranayaka756@gmail.com>
2024-10-13 16:19:54 +0530

    user entity updated

--- src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/entity/UserEntity.java
+++ src/main/java/lk/ijse/gdse/aad68/NoteTakerV2/entity/UserEntity.java
@@ -5,10 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -27,19 +30,38 @@ public class UserEntity implements SuperEntity, UserDetails {
     private String profilePic;
     @OneToMany(mappedBy = "user")
     private List<NoteEntity> notes;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE" + role.name()));
+        return authorities;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 }
